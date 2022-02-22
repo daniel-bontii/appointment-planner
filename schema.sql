5147ef0040ecbaa5192c51bd9a2c74e26e4bbe38
daniel-bontii <daniel.bontii@amalitech.org>
2022-02-22 13:16:27 +0000

    corrected contacts table and added appointments table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
-CREATE TABLE appointments(
+CREATE TABLE contacts(
     id SERIAL PRIMARY KEY,
+    contact_name VARCHAR(50),
     contact VARCHAR(10),
     email VARCHAR(50)
+);
+
+CREATE TABLE appointments(
+    id SERIAL PRIMARY KEY,
+    appointment_title VARCHAR(50),
+    appointment_date DATE,
+    appointment_time TIME,
 );
