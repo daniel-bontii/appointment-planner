eccd6df9e7bfd54abc5cbf2f43f87d4ec1c25718
daniel-bontii <daniel.bontii@amalitech.org>
2022-02-22 22:35:02 +0000

    connected front end to delete contact route

--- client/src/components/tile/Tile.js
+++ client/src/components/tile/Tile.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 export const Tile = ({ tile }) => {
- 
+  const handleDelete = async(id) =>{
+    // e.preventDefault();
+    console.log('trying to delete')
+    try {
+      await fetch(`http://localhost:4001/aplanner/api/v1/contacts/${id}`,{
+        method: 'DELETE'
+      });
+      window.location = "/";    
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 
   return (
     <div className="tile-container">
@@ -11,7 +22,7 @@ export const Tile = ({ tile }) => {
         </p>
         
       ))}
-      
+      <button onClick={()=>handleDelete(tile.id)}>Delete</button>
     </div>
   );
 };
