b86427137fc357ed7d809f79f41361dedd204357
daniel-bontii <daniel.bontii@amalitech.org>
2022-02-22 17:33:54 +0000

    added get single contact backend functionality

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,5 +1,18 @@
 const pool = require("../db");
 
+exports.checkID = async (req, res, next, val) => {
+  try {
+    const id = await pool.query("SELECT id FROM contacts WHERE ID=$1", [val]);
+    if (id.rowCount < 1) {
+      return res.status(404).json("Non existent contact");
+    }
+    next();
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Internal Server error");
+  }
+};
+
 exports.getAllContacts = async (req, res) => {
   try {
     const contacts = await pool.query("SELECT * FROM contacts");
@@ -9,3 +22,19 @@ exports.getAllContacts = async (req, res) => {
     res.status(500).json("Internal Server error");
   }
 };
+
+exports.getContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const contac = await pool.query(
+      "SELECT * FROM contacts WHERE id=$1",
+      [id]
+    );
+    res.status(200).json(contac.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Internal Server error");
+  }
+};
+
+
--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const contactsController = require('../controllers/contactsController');
+const express = require("express");
+const contactsController = require("../controllers/contactsController");
 
 const router = express.Router();
+router.param('id', contactsController.checkID);
 
-router
-  .route('/')
-  .get(contactsController.getAllContacts)
-
+router.route("/").get(contactsController.getAllContacts);
+router.route("/:id").get(contactsController.getContact);
 
 module.exports = router;
