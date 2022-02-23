b8d337ffe2310dd93487f7c5d5225a13a0d4e0eb
daniel-bontii <daniel.bontii@amalitech.org>
2022-02-23 07:36:54 +0000

    set up appointments front end to  pick contacts from backend

--- client/src/components/contactPicker/ContactPicker.js
+++ client/src/components/contactPicker/ContactPicker.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
 
 export const ContactPicker = ({ name, onChange, contacts }) => {
+  const [dbcontacts, setDBContacts] = useState([]);
+
+  const getContacts = async () => {
+    try {
+      const res = await fetch("http://localhost:4001/aplanner/api/v1/contacts");
+      const jsonRes = await res.json();
+      setDBContacts(jsonRes);
+      console.log(dbcontacts);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
   return (
     <select name={name} onChange={onChange}>
       <option value={""} key={-1} selected="selected">
         No Contact Selected
       </option>
-      {contacts.map((contact) => {
+      {dbcontacts.map((contact) => {
         return (
-          <option value={contact} key={contact}>
-            {contact}
+          <option value={contact.id} key={contact.id}>
+            {contact.contact_name}
           </option>
         );
       })}
